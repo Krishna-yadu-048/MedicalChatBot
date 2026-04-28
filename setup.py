6a343817176cc441420b7aca35881864d793dee8
Krishna-yadu-048 <krishnayadu048@gmail.com>
2026-04-28 20:27:39 +0530

    project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "medical-chatbot",
+    version= "0.1.0",
+    author= "Dark_Rage",
+    author_email="infodiary086@gmail.com",
+    packages= find_packages(),
+    intstall_requires=[]
+)
